b56721b550fa4000ecd26b9f3a98bf7209e82e83
Andrey Buglin <buglin.andrey@gmail.com>
2022-11-20 11:25:41 +0300

    Sprint 3. Final project. Correction of review comments

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -136,24 +136,47 @@ void TestRelevanceSortingDocuments()
         server.AddDocument(doc_id_3, content_3, DocumentStatus::ACTUAL, ratings_3);
         const auto found_docs = server.FindTopDocuments("fluffy well-groomed cat"s);
         ASSERT_EQUAL(found_docs.size(), 3);
-        ASSERT_EQUAL(found_docs[0].id, doc_id_1);
-        ASSERT_EQUAL(found_docs[1].id, doc_id_3);
-        ASSERT_EQUAL(found_docs[2].id, doc_id_2);
+        ASSERT(found_docs[0].relevance > found_docs[1].relevance);
+        ASSERT(found_docs[1].relevance > found_docs[2].relevance);
     }
 }
 
 // Вычисление рейтинга документов.
 void TestRatingDocumentsCalc()
 {
-    const int doc_id = 3;
-    const string content = "fluffy cat fluffy tail"s;
-    const vector<int> ratings = { 1, 2, 3 };
-    // Рейтинг добавленного документа равен среднему арифметическому оценок документа.
+    const int doc_id_1 = 0;
+    const string content_1 = "fluffy cat fluffy tail"s;
+    const vector<int> ratings_1 = { 1, 2, 3 };
+
+    const int doc_id_2 = 1;
+    const string content_2 = "well-groomed dog expressive eyes"s;
+    const vector<int> ratings_2 = { -4, -5, -6 };
+
+    const int doc_id_3 = 2;
+    const string content_3 = "fluffy well-groomed cat in city"s;
+    const vector<int> ratings_3 = { 8, -9, 10 };
+    // Рейтинг добавленного документа равен среднему арифметическому положительных оценок документа.
     {
         SearchServer server;
-        server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
+        server.AddDocument(doc_id_1, content_1, DocumentStatus::ACTUAL, ratings_1);
         const auto found_docs = server.FindTopDocuments("fluffy tail"s);
         double expected_raiting = static_cast<double>((1 + 2 + 3) / 3);
+        ASSERT_EQUAL(found_docs[0].rating, expected_raiting);
+    }
+    // Рейтинг добавленного документа равен среднему арифметическому отрицательных оценок документа.
+    {
+        SearchServer server;
+        server.AddDocument(doc_id_2, content_2, DocumentStatus::ACTUAL, ratings_2);
+        const auto found_docs = server.FindTopDocuments("well-groomed dog"s);
+        double expected_raiting = static_cast<double>((-4 - 5 - 6) / 3);
+        ASSERT_EQUAL(found_docs[0].rating, expected_raiting);
+    }
+    // Рейтинг добавленного документа равен среднему арифметическому смешанных оценок документа.
+    {
+        SearchServer server;
+        server.AddDocument(doc_id_3, content_3, DocumentStatus::ACTUAL, ratings_3);
+        const auto found_docs = server.FindTopDocuments("fluffy cat"s);
+        double expected_raiting = static_cast<double>((8 - 9 + 10) / 3);
         ASSERT_EQUAL(found_docs[0].rating, expected_raiting);
     }
 }
@@ -198,26 +221,63 @@ void TestPredicateDocumentSearch()
 // Фильтрация результатов поиска с использованием статуса, задаваемого пользователем.
 void TestStatusDocumentSearch()
 {
-    const int doc_id = 3;
-    const string content = "fluffy well-groomed cat in city"s;
-    const vector<int> ratings = { 1, 2, 3 };
-    const DocumentStatus document_status = DocumentStatus::BANNED;
+    const int doc_id_1 = 1;
+    const string content_1 = "fluffy cat fluffy tail"s;
+    const vector<int> ratings_1 = { 1, 2, 3 };
+    const DocumentStatus document_status_1 = DocumentStatus::ACTUAL;
+
+    const int doc_id_2 = 2;
+    const string content_2 = "well-groomed dog expressive eyes"s;
+    const vector<int> ratings_2 = { 4, 5, 6 };
+    const DocumentStatus document_status_2 = DocumentStatus::IRRELEVANT;
+
+    const int doc_id_3 = 3;
+    const string content_3 = "fluffy well-groomed cat in city"s;
+    const vector<int> ratings_3 = { 7, 8, 9 };
+    const DocumentStatus document_status_3 = DocumentStatus::BANNED;
+
+    const int doc_id_4 = 4;
+    const string content_4 = "fluffy cat in village"s;
+    const vector<int> ratings_4 = { 10, 11, 12 };
+    const DocumentStatus document_status_4 = DocumentStatus::REMOVED;
     // Документы с подходящими словами не выдаются с не актуальным статусом
     {
         SearchServer server;
-        server.SetStopWords("in the"s);
-        server.AddDocument(doc_id, content, document_status, ratings);
-        const auto found_docs = server.FindTopDocuments("well-groomed cat in village"s, DocumentStatus::ACTUAL);
+        server.AddDocument(doc_id_1, content_1, document_status_1, ratings_1);
+        const auto found_docs = server.FindTopDocuments("well-groomed cat in village"s, DocumentStatus::IRRELEVANT);
         ASSERT(found_docs.empty());
     }
-    // Документы с подходящими словами и подходящим статусом выдаются
+    // Документы с подходящими словами и подходящим статусом ACTUAL выдаются
     {
         SearchServer server;
-        server.SetStopWords("in the"s);
-        server.AddDocument(doc_id, content, DocumentStatus::BANNED, ratings);
+        server.AddDocument(doc_id_1, content_1, document_status_1, ratings_1);
+        const auto found_docs = server.FindTopDocuments("well-groomed cat in village"s, DocumentStatus::ACTUAL);
+        ASSERT(!found_docs.empty());
+        ASSERT_EQUAL(found_docs[0].id, doc_id_1);
+    }
+    // Документы с подходящими словами и подходящим статусом IRRELEVANT выдаются
+    {
+        SearchServer server;
+        server.AddDocument(doc_id_2, content_2, document_status_2, ratings_2);
+        const auto found_docs = server.FindTopDocuments("well-groomed cat in village"s, DocumentStatus::IRRELEVANT);
+        ASSERT(!found_docs.empty());
+        ASSERT_EQUAL(found_docs[0].id, doc_id_2);
+    }
+    // Документы с подходящими словами и подходящим статусом BANNED выдаются
+    {
+        SearchServer server;
+        server.AddDocument(doc_id_3, content_3, document_status_3, ratings_3);
         const auto found_docs = server.FindTopDocuments("well-groomed cat in village"s, DocumentStatus::BANNED);
         ASSERT(!found_docs.empty());
-        ASSERT_EQUAL(found_docs[0].id, doc_id);
+        ASSERT_EQUAL(found_docs[0].id, doc_id_3);
+    }
+    // Документы с подходящими словами и подходящим статусом REMOVED выдаются
+    {
+        SearchServer server;
+        server.AddDocument(doc_id_4, content_4, document_status_4, ratings_4);
+        const auto found_docs = server.FindTopDocuments("well-groomed cat in village"s, DocumentStatus::REMOVED);
+        ASSERT(!found_docs.empty());
+        ASSERT_EQUAL(found_docs[0].id, doc_id_4);
     }
 }
 
@@ -237,8 +297,8 @@ void TestRelevanceDocumentCalc()
         server.AddDocument(doc_id_1, content_1, DocumentStatus::ACTUAL, ratings_1);
         server.AddDocument(doc_id_2, content_2, DocumentStatus::ACTUAL, ratings_2);
         const auto found_docs = server.FindTopDocuments("well-groomed cat"s);
-        double expected_relevance = 0.23104906018664842;
-        ASSERT_EQUAL(found_docs[0].relevance, expected_relevance);
+        double expected_relevance = 0.231049;
+        ASSERT(abs(found_docs[0].relevance - expected_relevance) < 1e-6);
     }
 }
 
